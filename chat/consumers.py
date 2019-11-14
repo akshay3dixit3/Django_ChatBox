d888ca5d3250f183d903adac913406c1f4066803
Akshay Dixit <akshay.dixit@tessact.com>
2019-11-14 23:27:49 +0530

    new file:   chat/admin.py 	new file:   chat/apps.py 	new file:   chat/consumers.py 	new file:   chat/models.py 	new file:   chat/routing.py 	new file:   chat/templates/chat/index.html 	new file:   chat/templates/chat/room.html 	new file:   chat/tests.py 	new file:   chat/urls.py 	new file:   chat/views.py 	new file:   justchat/routing.py 	new file:   justchat/settings.py 	new file:   justchat/urls.py 	new file:   justchat/wsgi.py 	new file:   manage.py

---
+++ chat/consumers.py
@@ -0,0 +1,20 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
